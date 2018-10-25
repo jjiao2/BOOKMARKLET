65117d6528a46ef8ee3e8c026f183c10b1789c55
jjiao2 <jjiao2@saic.edu>
2018-10-25 14:17:54 -0500

    ah

--- xxx.js
+++ xxx.js
@@ -4,13 +4,10 @@
 
     let paragraphs = document.getElementsByTagName( 'p' );
 
-    for ( let i = 0; i < paragraphs.length; i++ ) {
-
-        paragraphs[i].innerHTML = 'A STORY. FADE IN.';
-
-    }
-
+    ( let i = 0; i < paragraphs.length; i++ ) {
+paragraphs[i].innerHTML = "The sky is bright but covered by dense clouds. the street in front a city hotel is filled with all kinds of white-collar workers, carrying portable briefcases, and people walking in the crowd. the traffic on the road is bad. a cab drives away from the hotel. in the cab, dales miles, 19, a brown haired young man with tanned skin wears a grey hoodie, and an olive-green scarf, black coat sits on the back sit. he looks tired."; }
 } ) ();
+
 
 // (function() {
 //
