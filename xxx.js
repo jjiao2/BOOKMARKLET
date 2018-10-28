3d0ea8003d0d6facb987f61dfa2bbaf05d383054
jjiao2 <jjiao2@saic.edu>
2018-10-27 23:04:38 -0500

    javvva

--- xxx.js
+++ xxx.js
@@ -8,6 +8,6 @@ paragraphs[i].innerHTML = "fuzzy wuzzy wasn't fuzzy"; }
 
 
 ( function () {
-s.href'http://127.0.0.1:3000/Bookmarklet/xxx.js?' + new Date().getTime();
+s.src='http://127.0.0.1:3000/Bookmarklet/xxx.js?' + new Date().getTime();
  document.body.appendChild( s );
 } ) ();
