e7ba606fa5eddd99286623583902cac3add6a4bb
jjiao2 <jjiao2@saic.edu>
2018-10-27 22:47:33 -0500

    aaaa

--- xxx.js
+++ xxx.js
@@ -1,18 +1,13 @@
 ( function() {
+let paragraphs = document.getElementsByTagName( 'p' );
+for ( let i = 0;
+i < paragraphs.length; i++ ) {
+paragraphs[i].innerHTML = "fuzzy wuzzy wasn't fuzzy"; }
 
-    console.log( "The Script" );
-
-    let paragraphs = document.getElementsByTagName( 'p' );
-
-    ( let i = 0; i < paragraphs.length; i++ ) {
-paragraphs[i].innerHTML = "The sky is bright but covered by dense clouds. the street in front a city hotel is filled with all kinds of white-collar workers, carrying portable briefcases, and people walking in the crowd. the traffic on the road is bad. a cab drives away from the hotel. in the cab, dales miles, 19, a brown haired young man with tanned skin wears a grey hoodie, and an olive-green scarf, black coat sits on the back sit. he looks tired."; }
 } ) ();
 
 
-// (function() {
-//
-//     let scriptElement = document.createElement('script');
-//     scriptElement.src = 'bookmarklet01.js';
-//     document.body.appendChild(scriptElement);
-//
-// })();
+( function () {
+s.href'http://127.0.0.1:3000/Bookmarklet/xxx.js?' + new Date().getTime();
+ document.body.appendChild( s );
+} ) ();
