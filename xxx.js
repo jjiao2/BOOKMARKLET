db5017a791fdaead065801f4295afc92d8c7a1dd
jjiao2 <jjiao2@saic.edu>
2018-11-08 13:51:05 -0600

    fixed?

--- xxx.js
+++ xxx.js
@@ -1,13 +1,18 @@
 ( function() {
-let paragraphs = document.getElementsByTagName( 'p' );
-for ( let i = 0;
-i < paragraphs.length; i++ ) {
-paragraphs[i].innerHTML = "fuzzy wuzzy wasn't fuzzy"; }
+  let paragraphs = document.getElementsByTagName( 'p' );
+
+  for ( let i = 0; i < paragraphs.length; i++ ) {
+
+    paragraphs[i].innerHTML = "?????";
+
+  }
+
+  console.log("why?");
 
 } ) ();
 
 
-( function () {
-s.src='http://127.0.0.1:3000/Bookmarklet/xxx.js?' + new Date().getTime();
- document.body.appendChild( s );
-} ) ();
+// ( function () {
+// s.src='http://127.0.0.1:3000/Bookmarklet/.xxx.js?' + new Date().getTime();
+//  document.body.appendChild( s );
+// } ) ();
