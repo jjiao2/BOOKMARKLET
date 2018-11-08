b089d56818854c072be69258eefaa2ba250baa38
jjiao2 <jjiao2@saic.edu>
2018-11-08 14:03:03 -0600

    did fixed!!

--- xxx.js
+++ xxx.js
@@ -3,7 +3,7 @@
 
   for ( let i = 0; i < paragraphs.length; i++ ) {
 
-    paragraphs[i].innerHTML = "?????";
+    paragraphs[i].innerHTML = "Dear, dearest, dead, could you hold me in your hands?";
 
   }
 
@@ -16,3 +16,8 @@
 // s.src='http://127.0.0.1:3000/Bookmarklet/.xxx.js?' + new Date().getTime();
 //  document.body.appendChild( s );
 // } ) ();
+
+
+
+
+(function(){let s;s.src='http://127.0.0.1:3000/Bookmarklet/.xxx.js';document.body.appendChild(s);})();
